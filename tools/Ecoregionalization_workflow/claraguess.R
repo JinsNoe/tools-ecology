27e11283b3fe720b18528e38fd89c25c488a1807
JinsNoe <jinsnoe@gmail.com>
2025-05-30 14:24:24 +0200

    clustering on the right data: an  abundance matrix, tests updated for the correct results

--- tools/Ecoregionalization_workflow/claraguess.R
+++ tools/Ecoregionalization_workflow/claraguess.R
@@ -16,7 +16,7 @@ if (length(args)==0) {
 #load data
 enviro_path <- args[1]
 preds_path <- args[2]
-taxas_path <- args[3]
+taxa_path <- args[3]
 type <- args[4]
 k <- as.integer(args[5])
 metric <- args[6]
@@ -32,9 +32,22 @@ for (i in 1:length(data_split[[1]])) {
   remove(df)
 }
 
-names(preds.data) <- c("lat", "long", "pred", "taxa")
+names(preds.data) <- c("lat", "long", "pred", "taxon")
 
-development_traits <- str_split(readLines(taxas_path), "\t")
+##List of modelled taxa used for clustering
+taxa_list <- tv <- read.table(taxa_path, dec=".", sep=" ", header=F, na.strings = "NA") 
+names(taxa_list) <- c("a")
+
+preds.data <- preds.data[which(preds.data$taxon %in% taxa_list$a),]
+
+#format data
+abondance_mat <- matrix(preds.data$pred, nrow = nrow(env.data), ncol = nrow(preds.data) / nrow(env.data))
+abondance_mat <- data.frame(abondance_mat)
+
+message("abondance_mat", abondance_mat)
+message("colnames(abondance_mat)", colnames(abondance_mat))
+message("unique(preds.data$taxon)", unique(preds.data$taxon))
+names(abondance_mat) <- unique(preds.data$taxon)
 
 #select the clara model with the optimal number of clusters
 model <- NULL
@@ -44,18 +57,18 @@ if (type == "auto") {
   models <- list()
   
   for (i in 2:k) {
-    models[[i]] <- clara(preds.data$pred, i, metric = metric, samples = samples, stand = TRUE)
+    models[[i]] <- clara(abondance_mat, i, metric = metric, samples = samples, stand = TRUE)
     sih_scores[i] <- models[[i]]$silinfo$avg.width
   }
   png("sih_scores.png")
   plot(2:k, sih_scores[2:k], type = "b", xlab = "Number of clusters", ylab = "SIH index")
   dev.off()
   
-  best_k <- which.max(sih_scores[3:k]) + 2
+  best_k <- which.max(sih_scores[2:k]) + 1
   model <- models[[best_k]]
   k <- best_k
 } else {
-  model <- clara(preds.data$pred, k, metric = metric, samples = samples, stand = TRUE)
+  model <- clara(abondance_mat, k, metric = metric, samples = samples, stand = TRUE)
 }
 
 #saving results
@@ -63,13 +76,9 @@ png("silhouette_plot.png")
 plot(silhouette(model), main = paste("Silhouette plot for k =", k))
 dev.off()
 
-data.test <- matrix(preds.data$pred, nrow = nrow(env.data), ncol = nrow(preds.data) / nrow(env.data))
-data.test <- data.frame(data.test)
-names(data.test) <- unique(preds.data$development)
-
-full.data <- cbind(preds.data[1:nrow(data.test), 1:2], model$clustering)
+full.data <- cbind(preds.data[1:nrow(abondance_mat), 1:2], model$clustering)
 names(full.data) <- c("lat", "long", "cluster")
-full.data <- cbind(full.data, data.test, env.data[, 3:ncol(env.data)])
+full.data <- cbind(full.data, abondance_mat, env.data[, 3:ncol(env.data)])
 
 write.table(full.data[1:3], file = "data_cluster.tabular", quote = FALSE, sep = "\t", row.names = FALSE)
 write.table(full.data, file = "clustered_taxas_env.tabular", quote = FALSE, sep = "\t", row.names = FALSE)
